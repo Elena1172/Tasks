41a9dcb7c0b7e1018ebc0c98130c6442e5c755ac
Elena <zagre8elena@yandex.ru>
2022-10-14 16:45:05 +0300

    added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа выводит ряд четных чисел от 1 до набранного пользователем числа
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (number > 0){
+    while(count <= number){
+        if(count % 2 == 0){
+            
+            Console.Write(count + " ");
+        }
+        count ++;
+    }
+
+}
+else{
+    Console.Write("Вы ввели ненатуральное число");
+}
