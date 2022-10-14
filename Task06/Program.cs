584e59e00fd2bdce483b892e48831b8b64fe2f5d
Elena <zagre8elena@yandex.ru>
2022-10-14 16:26:34 +0300

    added Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,10 @@
+﻿// программа определяет введенное пользователем число является 
+//четным либо нечетным
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 == 0){
+    Console.Write($"Число {number} четное.");
+}
+else{
+    Console.Write($"Число {number} нечетное.");
+}
