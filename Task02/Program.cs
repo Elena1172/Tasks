70c3c9c5e0ff10a685f0255ca2bed72d8c1af84d
Elena <zagre8elena@yandex.ru>
2022-10-14 15:56:22 +0300

    added Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Число a больше числа b.");
+}
+else if (b > a)
+{
+    Console.WriteLine("Число b больше числа a.");
+}
+else
+{
+    Console.WriteLine("Число a равно числу b.");
+}
