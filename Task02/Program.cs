491a262b8b66e27eeba176e19c58a89efc10af50
Elena <zagre8elena@yandex.ru>
2022-10-14 16:13:04 +0300

    added Task04 and change Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// программа сравнивает два числа
 Console.Write("Введите число a: ");
 double a = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите число b: ");
---
+++ Task04/Program.cs
@@ -0,0 +1,13 @@
+﻿// программа находит наибольшее число из трех введенных чисел
+Console.WriteLine("Введите три числа:");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+double max = a;
+if(b > a){
+    max = b;
+}
+if(c > max){
+    max = c;
+}
+Console.WriteLine($"Наибольшее значение из трех чисел = {max}");
